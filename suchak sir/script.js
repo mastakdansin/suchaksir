ed296f320a25ef20ddb4f8a28430041ad796ce11
mastakdansin <mastakdansin@gmail.com>
2024-12-10 10:18:13 -0800

    Add files via upload

---
+++ suchak sir/script.js
@@ -0,0 +1,7 @@
+// Toggle the navigation menu on and off
+const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
